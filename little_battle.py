6f47d823ed8815d9d9b9b6de184b88bf145f6668
Baiwei PC <f243165417@gmail.com>
2021-05-12 23:56:17 +1200

    Completed part of the import config data check, up to "Check valid resource position (make sure they are not out of the frame) ". implemented helper function for incoming check of the reserved position.

--- little_battle.py
+++ little_battle.py
@@ -1,6 +1,8 @@
 import sys
 
 # Please implement this function according to Section "Read Configuration File"
+
+
 def load_config_file (filepath):
     # It should return width, height, waters, woods, foods, golds based on the file
     # Complete the test driver of this function in file_loading_test.py
@@ -16,7 +18,7 @@ def load_config_file (filepath):
 
     # ------------------------------------check file format------------------------------------
 
-    # remove all empty lines
+    # remove all empty lines and store the data in content_list
     for line in contents:
         if not line.strip():  # empty sequences detected
             continue
@@ -32,11 +34,12 @@ def load_config_file (filepath):
         if line.split()[0] not in label_check:
             raise SyntaxError("Invalid Configuration File: format error! unexpected label detected!")
 
-    # ------------------------------------check file content-----------------------------
-    for lines in contents_list:
-        print(lines.split())
+    # ------------------------------------check the content of Frame-----------------------------
+    # debug
+    # for lines in contents_list:
+    #     print(lines.split())
 
-    # check the content of Frame
+    # check there are only one set of weight and height
     frame = contents_list[0].split()
     if len(frame) != 2:
         raise SyntaxError("Invalid Configuration File: frame should be in format width x height")
@@ -46,7 +49,6 @@ def load_config_file (filepath):
         width_and_height = frame[1].split("x")
     else:
         raise SyntaxError("Invalid Configuration File: frame should be in format width x height")
-    print(width_and_height)
 
     # check for weight x height format
     if '' in width_and_height or len(width_and_height) > 2:
@@ -56,12 +58,80 @@ def load_config_file (filepath):
     if not 5 <= int(width_and_height[0]) <= 7 or not 5 <= int(width_and_height[1]) <= 7:
         raise ArithmeticError("Invalid Configuration File: width and height should range from 5 to 7")
 
+    # store width and height
+    width = int(width_and_height[0])
+    height = int(width_and_height[1])
+
+    # -------------------------check the second line to the last line-----------------------------
+    second_to_last_lines = contents_list[1:]
+    print(second_to_last_lines)
+
+    # check for non int characters and turn second_to_last_lines into a binary list
+    for i in range(len(second_to_last_lines)):
+        second_to_last_lines[i] = second_to_last_lines[i].split()   # convert lines into lists
+        for num in second_to_last_lines[i][1:]:
+            if detect_int(num):
+                continue
+            else:
+                raise ValueError("Invalid Configuration File: " + second_to_last_lines[i][0][:-1] + "contains non "
+                                                                                                    "integer "
+                                                                                                    "characters!")
+    # store the resource locations
+    for resource_lists in second_to_last_lines:
+        resource_name = resource_lists[0][:-1]
+        resource_position = resource_lists[1:]
+
+        if resource_name == "Water":
+            waters = resource_position
+        elif resource_name == "Wood":
+            woods = resource_position
+        elif resource_name == "Food":
+            foods = resource_position
+        elif resource_name == "Gold":
+            golds = resource_position
+
+    print(waters)
+    print(woods)
+    print(foods)
+    print(golds)
+
+    # check for odd number of elements
+    for line in second_to_last_lines:
+        if len(line) % 2 == 0:
+            raise ValueError("Invalid Configuration File: " + line[0][:-1] + " has an odd number of elements!")
+
+    # check for valid position
+    for line in second_to_last_lines:
+        # check widths are valid
+        for num in line[1::2]:
+            if not 0 <= int(num) <= width - 1:
+                raise ArithmeticError("Invalid Configuration File: " + line[0][:-1] + " contains a position that "
+                                                                                      "is out of map.")
+        # check height are valid
+        for num_h in line[2::2]:
+            if not 0 <= int(num_h) <= height - 1:
+                raise ArithmeticError("Invalid Configuration File: " + line[0][:-1] + " contains a position that "
+                                                                                      "is out of map.")
+    # check for reserved position
+
+
 
     print("Success!")
 
     return width, height, waters, woods, foods, golds
 
 
+# helper function for detecting non integer character
+def detect_int(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+# helper function for checking reserved position
+def check_reserved_p(list_p, width, height):
+    
 
 
 if __name__ == "__main__":
@@ -69,6 +139,6 @@ if __name__ == "__main__":
         print("Usage: python3 little_battle.py <filepath>")
         sys.exit()
     try:
-        width, height, waters, woods, foods, golds = load_config_file(sys.argv[1])
+        width_new, height_new, waters_new, woods_new, foods_new, golds_new = load_config_file(sys.argv[1])
     except FileNotFoundError:
         print("Warning: no such file founded")
